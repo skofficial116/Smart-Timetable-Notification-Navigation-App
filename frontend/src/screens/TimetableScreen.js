6c4314df94512abfdbdc1b132d42f59daf610f93
Sachin Kumar <146352431+skofficial116@users.noreply.github.com>
2025-03-04 10:14:52 +0530

    Create TimetableScreen.js

---
+++ frontend/src/screens/TimetableScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Timetable from "../components/Timetable/Timetable";
+
+const TimetableScreen = () => {
+    return (
+        <div>
+            <h2>Timetable</h2>
+            <Timetable />
+        </div>
+    );
+};
+
+export default TimetableScreen;

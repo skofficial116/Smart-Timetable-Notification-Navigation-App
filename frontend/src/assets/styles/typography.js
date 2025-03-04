548931ca3aeda351d60460fcdd657901c33e071d
Sachin Kumar <146352431+skofficial116@users.noreply.github.com>
2025-03-04 10:31:46 +0530

    Create typography.js

---
+++ frontend/src/assets/styles/typography.js
@@ -0,0 +1,26 @@
+import { StyleSheet } from "react-native";
+
+const typography = StyleSheet.create({
+    header: {
+        fontSize: 24,
+        fontFamily: "Roboto-Bold",
+        color: "#333333",
+    },
+    subHeader: {
+        fontSize: 20,
+        fontFamily: "Roboto-Regular",
+        color: "#666666",
+    },
+    body: {
+        fontSize: 16,
+        fontFamily: "Roboto-Regular",
+        color: "#333333",
+    },
+    caption: {
+        fontSize: 12,
+        fontFamily: "Roboto-Regular",
+        color: "#999999",
+    },
+});
+
+export default typography;

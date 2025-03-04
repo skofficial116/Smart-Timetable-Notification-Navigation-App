c4eabdc57cb48ad4284170ee3ee782b7349b2d53
Sachin Kumar <146352431+skofficial116@users.noreply.github.com>
2025-03-04 10:13:47 +0530

    Create LoginScreen.js

---
+++ frontend/src/screens/LoginScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Login from "../components/Auth/Login";
+
+const LoginScreen = () => {
+    return (
+        <div>
+            <h2>Login</h2>
+            <Login />
+        </div>
+    );
+};
+
+export default LoginScreen;

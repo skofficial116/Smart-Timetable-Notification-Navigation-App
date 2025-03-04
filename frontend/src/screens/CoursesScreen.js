d18b26828c4d73d92342661771bce5eac8cf8466
Sachin Kumar <146352431+skofficial116@users.noreply.github.com>
2025-03-04 10:15:08 +0530

    Create CoursesScreen.js

---
+++ frontend/src/screens/CoursesScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import CourseList from "../components/Courses/CourseList";
+
+const CoursesScreen = () => {
+    return (
+        <div>
+            <h2>Courses</h2>
+            <CourseList />
+        </div>
+    );
+};
+
+export default CoursesScreen;

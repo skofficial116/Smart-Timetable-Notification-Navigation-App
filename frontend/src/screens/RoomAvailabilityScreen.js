2839c38a764e3d24267435f02303d8e17e44db65
Sachin Kumar <146352431+skofficial116@users.noreply.github.com>
2025-03-04 10:15:42 +0530

    Create RoomAvailabilityScreen.js

---
+++ frontend/src/screens/RoomAvailabilityScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import RoomAvailability from "../components/Rooms/RoomAvailability";
+
+const RoomAvailabilityScreen = () => {
+    return (
+        <div>
+            <h2>Room Availability</h2>
+            <RoomAvailability />
+        </div>
+    );
+};
+
+export default RoomAvailabilityScreen;
